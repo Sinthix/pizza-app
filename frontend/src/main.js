68552c3c4eb02f22106a09dca28b1a2448505bbe
Sinthix <ivo.ivan89@hotmail.com>
2024-12-10 20:38:22 +0000

    feat: adds pinia state management

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import './style.css'
 import App from './App.vue'
 import i18n from './i18n';
@@ -6,4 +7,6 @@ import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-createApp(App).use(router).use(i18n).mount('#app')
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(router).use(i18n).mount('#app')
